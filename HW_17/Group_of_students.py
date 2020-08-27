84106fc22cdfcafff1cf5fa9ace749e1899d48cb
Katerina <katerinna713@gmail.com>
2020-08-27 11:06:14 +0300

    Cleared code

--- HW_17/Group_of_students.py
+++ HW_17/Group_of_students.py
@@ -1,14 +1,13 @@
 class GroupOfStudents:
 
     def __init__(self, student_list):
-
         self.student_list = student_list
 
     @property
     def group_average(self):
         sum_gr = 0
-        for i in range(0, len(self.student_list)):
-            sum_gr = sum_gr + self.student_list[i].get_grades()
+        for student in self.student_list:
+            sum_gr = sum_gr + student.grades
         return sum_gr / len(self.student_list)
 
     @property
@@ -18,10 +17,10 @@ class GroupOfStudents:
 
         average = self.group_average
 
-        for i in range(0, len(self.student_list)):
+        for student in self.student_list:
 
-            if self.student_list[i].get_grades() < average:
-                names.append(self.student_list[i].get_name())
+            if student.grades < average:
+                names.append(student)
 
         return names
 
@@ -30,16 +29,17 @@ class GroupOfStudents:
 
         max = 0
 
-        for i in range(0, len(self.student_list)):
+        for student in self.student_list:
 
-            if self.student_list[i].get_grades() > max:
-                max = self.student_list[i].get_grades()
+            if student.grades > max:
+                max = student.grades
 
         students = []
 
-        for i in range(0, len(self.student_list)):
-            if self.student_list[i].get_grades() == max:
-                students.append(self.student_list[i].get_name())
+        for student in self.student_list:
+
+            if student.grades == max:
+                students.append(student)
 
         max_dict = {max: students}
         return max_dict
@@ -56,12 +56,18 @@ class GroupOfStudents:
         for key in max_dict:
 
             temp = max_dict[key]
+            # with open('students_enrolled_to_budget.txt', 'w+') as f:
+            #     for name in list(temp):
+            #         f.writelines(f'{name}\n')
             for name in list(temp):
                 print(f'\t {name}')
 
     def students_dropped_out(self):
         print(f'Students dropped out of the group are: ')
         students = self.students_below_average
+        # with open('students_dropped_out.txt', 'w+') as f:
+        #     for student in students:
+        #         f.write(f'{student}\n')
         for student in students:
             print(f'\t {student}')
 
@@ -74,12 +80,6 @@ class Student:
 
     def __repr__(self):
         return f'Student {self.name}, grades {self.grades}'
-
-    def get_name(self):
-        return self.name
-
-    def get_grades(self):
-        return self.grades
 
 
 def main():
